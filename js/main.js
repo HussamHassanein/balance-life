4d0c094fec5a21d7fd2c1ff1509839942a837acd
Hussam Hassanein <hussamh@kth.se>
2017-11-16 15:11:40 +0100

    add editable text

--- js/main.js
+++ js/main.js
@@ -1,15 +1,32 @@
 
 function showtext(){
  var text = document.getElementById("txtarea");
- var div = document.createElement('div');
- div.innerHTML = text.value;
- //div.setAttribute('class', 'myclass'); // and make sure myclass has some styles in css
- document.body.appendChild(div);
+ var div = document.createElement("div");
+ //var x = document.createElement("P");
+div.setAttribute('contenteditable', 'true');
+//div.setAttribute("style", "mystyle");
+// and make sure myclass has some styles in css
 
-  //var showarea = document.getElementById("adduserdata");
+div.innerHTML = text.value;
+document.body.appendChild(div);
+//document.body.appendChild(x);
+//document.getElementById(div).contentEditable = "true";
+//x.innerHTML=text.value;
+
+ //document.querySelector("[contenteditable=true]");
+ //var showarea = document.getElementById("adduserdata");
  //showarea.innerHTML=text.value;
 
 }
+
+
+function myFunction() {
+    var str=document.getElementById("txt").value;
+    var n=str.replace("");
+    var m=n.replace();
+    document.getElementById("txt").value=m;
+}
+
 function relocate_home()
 {
      location.href = "main.html";
